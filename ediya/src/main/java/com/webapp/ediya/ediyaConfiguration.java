9b1f5bf52a7961f258c98af6f55cbb331c0fcbb8
sanwalkailash@gmail.com <>
2017-11-21 09:58:31 +0530

    app init

---
+++ ediya/src/main/java/com/webapp/ediya/ediyaConfiguration.java
@@ -0,0 +1,10 @@
+package com.webapp.ediya;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class ediyaConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
