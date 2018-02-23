34928528359773c6d271ea4f3a537b8116f05434
kailash-cd <kailash@chalkdigital.com>
2018-02-23 17:09:14 +0530

    updates

--- ediya/src/main/java/com/webapp/ediya/resources/HelloResource.java
+++ ediya/src/main/java/com/webapp/ediya/resources/HelloResource.java
@@ -1,18 +1,27 @@
 package com.webapp.ediya.resources;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Map;
 
-@Path("/hello")
+@Path("/")
 public class HelloResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String getGreeting() {
-        return "Hello world!";
+    public String getGreeting() throws JsonProcessingException {
+        Map<String,String> apiDocs= new HashMap<>();
+        apiDocs.put("Info","Hi There, Following is the list of api");
+        apiDocs.put("1","/news/everyday/api_key?date=current_date");
+        apiDocs.put("2","coming soon..");
+        return new ObjectMapper().writeValueAsString(apiDocs);
     }
 
     @POST
